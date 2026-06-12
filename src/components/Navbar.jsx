3d94f8934fe7e0c77420b2f83494b7520f367811
ocampojkq <ocampo.jessekit1@gmail.com>
2026-06-12 19:05:21 +0800

    add icon facebook and github

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,30 @@
-import { FaBriefcase } from "react-icons/fa";
+import { FaGithubSquare } from "react-icons/fa";
+import { FaFacebookSquare } from "react-icons/fa";
 
 export default function Navbar() {
   return (
     <nav className="bg-white shadow-md">
       <div className="max-w-6xl mx-auto px-6 py-4 flex justify-between items-center">
         <div className="flex items-center gap-2">
-          <FaBriefcase className="text-blue-600 text-2xl" />
-
           <h1 className="text-2xl font-bold text-blue-600">VA Job Hub</h1>
         </div>
 
         <div className="flex gap-6">
-          <a href="#" className="hover:text-blue-600">
-            Home
+          <a
+            href="https://www.facebook.com/sspaniardd"
+            target="_blank"
+            rel="noreferrer"
+            className="hover:text-blue-600"
+          >
+            <FaFacebookSquare className="w-8 h-8" />
           </a>
-
-          <a href="#" className="hover:text-blue-600">
-            Categories
-          </a>
-
           <a
             href="https://github.com/ocampojkq/job-dashboard"
             target="_blank"
             rel="noreferrer"
             className="hover:text-blue-600"
           >
-            GitHub
+            <FaGithubSquare className="w-8 h-8" />
           </a>
         </div>
       </div>
